06e1906e38d646b6145cef2cb4c8a808a203863d
esteghlal1402 <202157739+esteghlal1402@users.noreply.github.com>
2025-09-03 20:26:01 +0330

    Create install.sh
    افزودن install.sh برای نصب خودکار ابزارهای رمزگذاری و منو

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "🔧 در حال نصب ابزارهای AhmadAI-Encrypt..."
+
+chmod +x encodeX.sh
+chmod +x decodeX.sh
+chmod +x menu-ahmad.sh
+
+echo "✅ همه فایل‌ها آماده اجرا شدن!"
+echo "برای شروع، دستور زیر رو بزن:"
+echo "./menu-ahmad.sh"
